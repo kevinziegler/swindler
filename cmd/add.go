d7d085f221fd4c9e3f647a4aec7afc4218e34594
Kevin Ziegler <kevin.ziegler3@gmail.com>
2018-11-03 15:55:03 -0500

    Implement barebones add command
    This doesn't do anything but echo back args yet, but we can extend it once we
    get a working adapter.

--- cmd/add.go
+++ cmd/add.go
@@ -16,7 +16,7 @@ package cmd
 
 import (
 	"fmt"
-
+	"errors"
 	"github.com/spf13/cobra"
 )
 
@@ -30,8 +30,16 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("Must supply an adapter and name for the new connection")
+		}
+
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		fmt.Printf("add called: adapter=%s, name=%s", args[0], args[1])
+		fmt.Println("")
 	},
 }
 
