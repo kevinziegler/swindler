25124c0c7ab70fae680aad9478e25fdc9e4bbd8e
Kevin Ziegler <kevin.ziegler3@gmail.com>
2018-11-03 18:22:52 -0500

    Implement thin demo for connection repo
    This lets us exercise the connection repo code via the dowop adapter

--- adapter/dowop.go
+++ adapter/dowop.go
@@ -2,6 +2,7 @@ package adapter
 
 import (
 	"fmt"
+	"swindler/connections"
 )
 
 type Dowop struct {}
@@ -9,4 +10,7 @@ type Dowop struct {}
 func (a Dowop) Add(thing string) {
 	fmt.Printf("dowop called: thing=%s", thing)
 	fmt.Println("")
+
+	repo := connections.FileRepository{File: "test.json"}
+	repo.Create()
 }
