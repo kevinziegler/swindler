763ce90c77e1de1fec32bb635686c703fed8f723
Kevin Ziegler <kevin.ziegler3@gmail.com>
2018-11-03 15:07:18 -0500

    Update root cmd with swindler verbiage.

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,17 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "swindler",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "This command line deserves a better class of connection manager.  I'm going to give it to them.",
+	Long: `Swindler is a database connection manager for your command line.  It stores
+connection information so you can easily manage connecting to multiple databases
+without having to remember various flags, arguments, credentials, and options
+when working with different tools or connecting with different databases.`,
+// 	Long: `A longer description that spans multiple lines and likely contains
+// examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+// Cobra is a CLI library for Go that empowers applications.
+// This application is a tool to generate the needed files
+// to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
